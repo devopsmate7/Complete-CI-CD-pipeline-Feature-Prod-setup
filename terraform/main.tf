53715d580b8249d556fc98183a7e86e06ff6b126
venkata srikanth solasa <venkatasrikanthsolasa@Venkatas-MacBook-Air.local>
2026-06-25 10:30:28 +0530

    modified managedby_tag

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,6 @@ resource "aws_s3_bucket" "app" {
     Name        = var.bucket_name
     Environment = var.environment_name
     Project     = "github-actions-cicd-demo"
-    ManagedBy   = "Terraform"
+    ManagedBy   = "manual"
   }
 }
